04f92c5816118f19986612270acb228787d680fb
aridsonf <filhoaridson@gmail.com>
2024-03-30 22:44:51 -0300

    feat(routes): all routes for the api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user/profile', [UserController::class, 'profile']);
+    Route::post('/logout', [LoginController::class, 'logout']);
+});
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function () {
+    return response()->json(['message' => 'Login page']);
+})->name('login');
+
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Test']);
 });
